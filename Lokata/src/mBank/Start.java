a1175ba54812bdde3f966b97cddf17285eb28a3c
magda.oleksy <oleksy.magda@gmail.com>
2017-10-27 19:17:43 +0200

    add ChromeDriver

--- Lokata/src/mBank/Start.java
+++ Lokata/src/mBank/Start.java
@@ -3,6 +3,7 @@ import org.openqa.selenium.WebDriver;
 //import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.PageFactory;
 import GUI.*;
 
@@ -12,9 +13,13 @@ public class Start {
 	
 	public static void main(String[] args){
 		//System.setProperty("webdriver.chrome.driver", "C:\\JARy\\chromedriver.exe");
-		WebDriver driver = new FirefoxDriver();
+		System.setProperty("webdriver.chrome.driver", "C:\\JARy\\chromedriver.exe");
+		 // DesiredCapabilities cap = DesiredCapabilities.firefox();
+	    //    cap.setCapability("marionette", true);
+	    //    cap.setBrowserName("firefox");
+		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
-		driver.get("http://www.mbank.pl");
+		driver.get("https://www.mbank.pl");
 		
         StartPage startPage = PageFactory.initElements(driver, StartPage.class);
         startPage.loginButtonClick();
