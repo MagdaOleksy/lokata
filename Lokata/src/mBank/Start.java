55e85784ee1752eca6549e56d1fd8aa437832ee9
magda.oleksy <oleksy.magda@gmail.com>
2017-10-28 21:31:33 +0200

    Update Start.java

--- Lokata/src/mBank/Start.java
+++ Lokata/src/mBank/Start.java
@@ -1,7 +1,7 @@
 package mBank;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+//import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.PageFactory;
 import GUI.*;
