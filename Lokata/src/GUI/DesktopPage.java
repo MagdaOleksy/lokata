7ae633ca3e8747d0a59c46570c9d169b273db047
magda.oleksy <oleksy.magda@gmail.com>
2017-10-28 21:22:55 +0200

    aaa

--- Lokata/src/GUI/DesktopPage.java
+++ Lokata/src/GUI/DesktopPage.java
@@ -1,5 +1,7 @@
 package GUI;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
@@ -7,8 +9,16 @@ public class DesktopPage {
 
 	WebDriver driver;
 	
+	@FindBy(how=How.XPATH, xpath ="//*[@id='main-nav']/ul/li[3]/a")
+	WebElement shoppingButton;
+	
+	public void clickShoppingButton(){
+		shoppingButton.click();
+	}
+	
 	public DesktopPage(WebDriver driver) {
 		this.driver = driver;
+		PageFactory.initElements(driver, this);
 	}
 
 }
--- Lokata/src/mBank/Start.java
+++ Lokata/src/mBank/Start.java
@@ -1,4 +1,6 @@
 package mBank;
+import java.util.Scanner;
+
 import org.openqa.selenium.WebDriver;
 //import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -11,19 +13,30 @@ public class Start {
 	
 	
 	
+	private static Scanner odczyt;
+
 	public static void main(String[] args){
-		//System.setProperty("webdriver.chrome.driver", "C:\\JARy\\chromedriver.exe");
+
 		System.setProperty("webdriver.chrome.driver", "C:\\JARy\\chromedriver.exe");
-		 // DesiredCapabilities cap = DesiredCapabilities.firefox();
-	    //    cap.setCapability("marionette", true);
-	    //    cap.setBrowserName("firefox");
+		System.out.println("Podaj login");
+		odczyt = new Scanner (System.in);
+		String login = odczyt.nextLine();
+		System.out.println("Podaj has³o");
+		String password = odczyt.nextLine();
+		
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://www.mbank.pl");
 		
         StartPage startPage = PageFactory.initElements(driver, StartPage.class);
         startPage.loginButtonClick();
+        
         LoginPage loginPage = PageFactory.initElements(driver, LoginPage.class);
-       // loginPage.logIn(login, password);
+        loginPage.logIn(login, password);
+        
+        DesktopPage desktopPage = new DesktopPage(driver);
+        desktopPage.clickShoppingButton();
+        
+        
 	}
 }
